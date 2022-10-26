eb7d16daef2df792c824c66af9e28904a1b94d8c
ec1ipse <wavesurf@gmail.com>
2022-10-26 20:11:31 +0200

    Create phone_numbers table

---
+++ database/migrations/2022_10_25_221108_create_phone_numbers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phone_numbers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('number')->unique();
+            $table->enum('phone_type', ['Home', 'Office', 'Mobile', 'Fax']);
+            $table->tinyInteger('is_default')->default(0);
+
+            $table->unsignedBigInteger('contact_id');
+            $table->foreign('contact_id')->references('id')->on('contacts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phone_numbers');
+    }
+};
