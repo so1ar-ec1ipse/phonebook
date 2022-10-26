8303d03680aecf0d47076aaa805e0b3520091099
ec1ipse <wavesurf@gmail.com>
2022-10-26 20:17:59 +0200

    Define and connect api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\ContactController;
+use App\Http\Controllers\Api\PhoneNumberController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('companies', CompanyController::class);
+Route::get('contacts/search/', [ContactController::class, 'search'])->name('contacts.search');
+Route::apiResource('contacts', ContactController::class);
+Route::apiResource('contacts.phone-numbers', PhoneNumberController::class);
